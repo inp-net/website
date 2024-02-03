1511b1289a3ddbc17fc6ccebb38fb37caeda630a
Ewen Le Bihan <hey@ewen.works>
2024-02-03 13:37:36 +0100

    Fix ci (round 2)

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 poetry run python projects/clone.py https://git.inpt.fr/api/graphql net7 $@
-ortfodb projects --scattered build to database.json --config ortfodb.yaml
+if [[ ! -f ortfodb ]]; then
+    curl -fsSL https://github.com/ortfo/db/releases/download/v0.1.0/ortfodb -o ortfodb
+fi
+chmod +x ortfodb
+./ortfodb projects --scattered build to database.json --config ortfodb.yaml
 poetry run python build_projects_page.py
