2c9aebd9d59674afd150e5462d56ad7737b37cad
Ewen Le Bihan <hey@ewen.works>
2024-02-03 13:40:48 +0100

    Fix CI (round 4)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 poetry run python projects/clone.py https://git.inpt.fr/api/graphql net7 $@
 if [[ ! -f ortfodb ]]; then
     curl -fsSL https://github.com/ortfo/db/releases/download/v0.1.0/ortfodb -o ortfodb
