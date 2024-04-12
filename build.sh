2d14590fe25f9dc8d1caa443b03b5f26cd88b304
Ewen Le Bihan <hey@ewen.works>
2024-04-13 00:25:07 +0200

    Update ortfodb version to 0.3.1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 poetry run python projects/clone.py https://git.inpt.fr/api/graphql "sur net7.dev" $@
 if [[ ! -f ortfodb ]]; then
-    curl -fsSL https://github.com/ortfo/db/releases/download/v0.1.0/ortfodb -o ortfodb
+    curl -fsSL https://github.com/ortfo/db/releases/download/v0.3.1/ortfodb -o ortfodb
 fi
 chmod +x ortfodb
 ./ortfodb projects --scattered build to database.json --config ortfodb.yaml
