40da180b2f8da4a720dd60d12f14272c8ce753a0
Ewen Le Bihan <hey@ewen.works>
2024-08-26 00:59:25 +0200

    Use ortfodb compiled against glibc 2.36

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+set -euxo pipefail
+
 poetry run python projects/clone.py https://git.inpt.fr/api/graphql "sur net7.dev" $@
 if [[ ! -f ortfodb ]]; then
-    curl -fsSL https://github.com/ortfo/db/releases/download/v0.3.1/ortfodb -o ortfodb
+    curl -fsSL https://github.com/ortfo/db/releases/download/v0.3.1/ortfodb-gblic2.36 -o ortfodb
 fi
 chmod +x ortfodb
 ./ortfodb projects --scattered build to database.json --config ortfodb.yaml
