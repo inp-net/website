d0c589bd5cd0938fd96aa726a5af306804351909
Ewen Le Bihan <hey@ewen.works>
2024-08-26 01:01:55 +0200

    Fix typo in build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 poetry run python projects/clone.py https://git.inpt.fr/api/graphql "sur net7.dev" $@
 if [[ ! -f ortfodb ]]; then
-    curl -fsSL https://github.com/ortfo/db/releases/download/v0.3.1/ortfodb-gblic2.36 -o ortfodb
+    curl -fsSL https://github.com/ortfo/db/releases/download/v0.3.1/ortfodb-glibc2.36 -o ortfodb
 fi
 chmod +x ortfodb
 ./ortfodb projects --scattered build to database.json --config ortfodb.yaml
